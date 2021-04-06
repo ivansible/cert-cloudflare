27b6ebb244b4edc4ef51e61ef10973e9292e2c19
Ivan Andreev <ivandeex@gmail.com>
2021-04-06 13:57:58 +0300

    re-add optional rsa support for cgpro compatibility

--- templates/etc/certbot-renew.sh
+++ templates/etc/certbot-renew.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec "{{ certbot_script }}" -q renew
+#set -x
+script="{{ certbot_script }}"
+{% for item in certbot_certificates %}
+{% set name = item.name %}
+{% set active = item.active |default(true) |bool %}
+{% set rsa = item.rsa |default(false) |bool %}
+{% if active %}
+$script -q renew --cert-name {{ name }}{% if rsa %} --key-type rsa{% endif %}
+
+{% endif %}
+{% endfor %}
